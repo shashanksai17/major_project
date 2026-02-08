f1d708a58c0eeb371e522b99de017040d9f8407c
Kotagiri Shashank Sai <shashanksai1703@gmail.com>
2026-02-08 21:41:33 +0530

    Fix property name from NODE_URL to NODE_ENV

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -5,5 +5,5 @@ dotenv.config({quiet:true});
 export const  ENV={
     PORT:process.env.PORT,
     DB_URL:process.env.DB_URL,
-    NODE_URL:process.env.NODE_ENV,
+    NODE_ENV:process.env.NODE_ENV,
 };
