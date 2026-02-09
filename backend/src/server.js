42460e1fa719ffb21120c64f9e16788007f28cd3
shashanksai <shashanksai1703@gmail.com>
2026-02-09 12:35:30 +0530

    auth-db-sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,22 @@ import express from  "express";
 import {ENV} from  "./lib/env.js";
 import path from "path";
 import {connectDB} from "./lib/db.js";
+import cors from "cors";
+import {serve} from "inngest/express";
+import { inngest, functions } from "./lib/inngest.js";
+
 const app=express();
 
 
-const _dirname=path.resolve();
+const __dirname=path.resolve();
+
+
+app.use(express.json());
+//
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
+
+app.use(".api/inngest",serve({client:inngest,functions}))
+
 
 app.get("/health",(req,res)=>{
     res.status(200).json({msg:"success from backend"});
@@ -18,10 +30,10 @@ app.get("/books",(req,res)=>{
 // make our app for deployment
 
 
-if(ENV.NODE_URL==="production"){
-    app.use(express.static(path.join(_dirname,"../frontend/vite-project/dist")));
+if(ENV.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/vite-project/dist")));
     app.get("/{*any}",(req,res)=>{
-        res.sendFile(path.join(_dirname,"../frontend/vite-project","dist","index.html"));
+        res.sendFile(path.join(__dirname,"../frontend/vite-project","dist","index.html"));
     })
 }
 
--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -3,8 +3,6 @@ import './App.css'
 import {SignedOut, SignInButton,SignedIn,UserButton,SignOutButton} from "@clerk/clerk-react"
 
 function App() {
-  
-
   return (
     <>
       <h1>Welcome to the app</h1>
