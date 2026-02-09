d0ad4d695917324d422257cee3b9e9683366bb19
Kotagiri Shashank Sai <shashanksai1703@gmail.com>
2026-02-09 16:25:10 +0530

    Fix syntax error in DB connection check

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,12 +3,12 @@ import {ENV} from "./env.js"
 
 export const connectDB=async()=>{
     try{
-        if(!ENV.DB_URL){{
+        if(!ENV.DB_URL){
             throw new Error("DB_URL is not defined in environment variables")
         }
        const conn= await mongoose.connect(ENV.DB_URL)
   console.log("connect to mongo") 
-   }
+   
 }
     catch(err){
         console.log(err);
