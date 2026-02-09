e0bf284917496d6ed37802615418ae27b30456d2
Kotagiri Shashank Sai <shashanksai1703@gmail.com>
2026-02-09 15:53:09 +0530

    Fix casing of clerkId in newUser object

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -14,7 +14,7 @@ const syncUser=inngest.createFunction(
         await connectDB();
         const {id,email_addresses,first_name,last_name,image_url}=event.data;
         const newUser={
-            clerkid:id,
+            clerkId:id,
             email:email_addresses[0]?.email_address,
             name:`${first_name || ""} ${last_name || ""}`.trim(),
             profileImage:image_url,
