cf678402b6fb95f246992bda79acc9fd9124654d
shashanksai <shashanksai1703@gmail.com>
2026-02-15 21:22:44 +0530

    frontend-tool-setup

--- frontend/vite-project/src/main.jsx
+++ frontend/vite-project/src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
+import { BrowserRouter } from 'react-router'
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
+
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -10,11 +13,16 @@ if (!PUBLISHABLE_KEY) {
   throw new Error('missing Clerk Publishable Key to the .env file')
 }
 
+  const queryClient =new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <App />
     </ClerkProvider>
+    </QueryClientProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
