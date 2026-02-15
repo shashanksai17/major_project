8bfa1de56d49925cc13f5075375080c79d7e6e6e
shashanksai <shashanksai1703@gmail.com>
2026-02-15 22:59:38 +0530

    Landing page added

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,18 +1,25 @@
 import {SignedOut, SignInButton,SignedIn,UserButton,SignOutButton} from "@clerk/clerk-react"
 import { Navigate,Route,Routes} from "react-router"
 import HomePage from "./pages/HomePage"
+import DashboardPage from "./pages/DashboardPage"
 import ProblemsPage from "./pages/ProblemsPage"
 import { useUser } from "@clerk/clerk-react"
 import { Toaster } from "react-hot-toast"
 function App() {
-  const  {isSignedIn}=useUser();
+  const  {isSignedIn,isLoaded}=useUser();
+  // this will get rid of flikering effect
+  if(!isLoaded){
+    return null;
+  }
+
   return (
     <>
     
     <Routes>
      {/* <h1 className="text-red-500 bg-orange-400 p-10 text-3xl">Welcome to the app</h1> */}
     
-    <Route path="/" element={<HomePage/>}/>
+    <Route path="/" element={!isSignedIn?<HomePage/>:<Navigate to= {"/dashboard"}/>}/>
+    <Route path="/dashboard" element={isSignedIn?<DashboardPage/>:<Navigate to= {"/"}/>}/>
     
     <Route path="/problems" element={isSignedIn?<ProblemsPage/>:<Navigate to= {"/"}/>}/>
 
